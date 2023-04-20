1d4efcd8a36489334af78482323f18a4d0f4b51d
HexDev <77530549+hex-developer@users.noreply.github.com>
2023-04-20 16:34:01 +0000

    try to add mp4 file type

--- service-worker.js
+++ service-worker.js
@@ -26,3 +26,42 @@ self.addEventListener('fetch', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    clients.claim()
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.endsWith('.mp4')) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          if (!response.ok) {
+            throw new TypeError('Bad response status');
+          }
+          const contentDisposition = response.headers.get('content-disposition');
+          if (!contentDisposition || !contentDisposition.includes('inline')) {
+            throw new TypeError('Invalid content-disposition header');
+          }
+          return response.blob();
+        })
+        .then((blob) => {
+          const videoUrl = URL.createObjectURL(blob);
+          return new Response('', {
+            status: 206,
+            headers: new Headers({
+              'Content-Type': 'text/html',
+              'Accept-Ranges': 'bytes',
+              'Content-Length': '0',
+              'Content-Range': `bytes 0-${blob.size - 1}/${blob.size}`,
+              'X-Content-Type-Options': 'nosniff',
+              'X-Robots-Tag': 'noindex'
+            }),
+            url: videoUrl
+          });
+        })
+    );
+  }
+});
+
